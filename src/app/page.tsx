e9b44c97d6ed296b81e214524b2d9c700feecc8f
정태양 <153086880+BitByte08@users.noreply.github.com>
2025-12-09 22:14:07 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <img src="http://localhost:8000/video_feed" width="640" height="480" />
+      <img src="http://localhost:8000/ai/video_feed" width="640" height="480" />
 
 
     </>
